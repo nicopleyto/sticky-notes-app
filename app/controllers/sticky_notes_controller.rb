d81799ed756e0521bc51be6e91c1b22bf18a4644
nicopleyto <nicopleyto@gmail.com>
2022-03-30 14:15:27 +0800

    removed jbuilder to simplify app

--- app/controllers/sticky_notes_controller.rb
+++ app/controllers/sticky_notes_controller.rb
@@ -1,14 +1,13 @@
 class StickyNotesController < ApplicationController
   before_action :set_sticky_note, only: %i[ show edit update destroy ]
 
-  # GET /sticky_notes or /sticky_notes.json
+  # GET /sticky_notes
   def index
     @sticky_notes = StickyNote.all
   end
 
-  # GET /sticky_notes/1 or /sticky_notes/1.json
-  def show
-  end
+  # GET /sticky_notes/1
+  def show; end
 
   # GET /sticky_notes/new
   def new
@@ -16,45 +15,32 @@ class StickyNotesController < ApplicationController
   end
 
   # GET /sticky_notes/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /sticky_notes or /sticky_notes.json
+  # POST /sticky_notes
   def create
     @sticky_note = StickyNote.new(sticky_note_params)
 
-    respond_to do |format|
-      if @sticky_note.save
-        format.html { redirect_to sticky_note_url(@sticky_note), notice: "Sticky note was successfully created." }
-        format.json { render :show, status: :created, location: @sticky_note }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @sticky_note.errors, status: :unprocessable_entity }
-      end
+    if @sticky_note.save
+      redirect_to sticky_note_url(@sticky_note), notice: "Sticky note was successfully created."
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /sticky_notes/1 or /sticky_notes/1.json
+  # PATCH/PUT /sticky_notes/1
   def update
-    respond_to do |format|
-      if @sticky_note.update(sticky_note_params)
-        format.html { redirect_to sticky_note_url(@sticky_note), notice: "Sticky note was successfully updated." }
-        format.json { render :show, status: :ok, location: @sticky_note }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @sticky_note.errors, status: :unprocessable_entity }
-      end
+    if @sticky_note.update(sticky_note_params)
+      redirect_to sticky_note_url(@sticky_note), notice: "Sticky note was successfully updated."
+    else
+      render :edit
     end
   end
 
   # DELETE /sticky_notes/1 or /sticky_notes/1.json
   def destroy
     @sticky_note.destroy
-
-    respond_to do |format|
-      format.html { redirect_to sticky_notes_url, notice: "Sticky note was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to sticky_notes_url, notice: "Sticky note was successfully destroyed."
   end
 
   private
