1fdfa60db8aa3d7ec66ce77f04229dee4328ba37
nicopleyto <nicopleyto@gmail.com>
2022-03-30 14:40:51 +0800

    added model associations and user id to stickynote

--- app/models/sticky_note.rb
+++ app/models/sticky_note.rb
@@ -1,4 +1,6 @@
 class StickyNote < ApplicationRecord
     validates :title, presence: true
     validates :body, length: {maximum: 280}
+
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_062130) do
+ActiveRecord::Schema.define(version: 2022_03_30_063347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2022_03_30_062130) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sticky_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 2022_03_30_062130) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sticky_notes", "users"
 end
