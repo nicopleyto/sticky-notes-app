3a08f4c51094433b301f98c65747b66332228da6
nicopleyto <nicopleyto@gmail.com>
2022-03-31 19:36:47 +0800

    added form validations and model validation for title

--- app/models/sticky_note.rb
+++ app/models/sticky_note.rb
@@ -1,5 +1,5 @@
 class StickyNote < ApplicationRecord
-    validates :title, presence: true
+    validates :title, presence: true, length: {maximum: 55}
     validates :body, length: {maximum: 280}
 
     belongs_to :user
