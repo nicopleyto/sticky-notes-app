615192439a2209baa36c98f6b68d49e6e504bf46
nicopleyto <nicopleyto@gmail.com>
2022-03-31 13:42:27 +0800

    added show view to public sticky notes

--- app/controllers/public_sticky_notes_controller.rb
+++ app/controllers/public_sticky_notes_controller.rb
@@ -2,4 +2,8 @@ class PublicStickyNotesController < ApplicationController
   def index
     @sticky_notes = StickyNote.all
   end
+
+  def show
+    @sticky_note = StickyNote.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'public_sticky_notes/index'
   devise_for :users
   resources :sticky_notes
+  resources :public_sticky_notes, only: [:index, :show]
   get 'home/about'
   root 'public_sticky_notes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
